9aa6712e9d991c55f7d1926b7404deaaa6191b3f
Jakub0821 <jakubmigda120@gmail.com>
2024-02-23 13:20:24 +0100

    change src and components

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,18 @@
+import { ContactForm } from "./components/ContactForm/ContactForm";
+import { Filter } from "./components/Filter/Filter";
+import { ContactList } from "./components/ContactList/ContactList";
+import css from "./App.module.css";
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <>
+      <div className={css.container}>
+        <h1>Phonebook</h1>
+        <ContactForm />
+        <h2>Contacts</h2>
+        <Filter />
+        <ContactList />
+      </div>
+    </>
   );
 };
